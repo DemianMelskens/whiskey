4e8aa91ff01977f7d6e4cee4abd4e203c21eaf41
Demian <dmelskens@quintor.nl>
2020-10-31 04:20:55 +0100

    worked further on menu bar

--- src/main/javascript/app/shared/shared.module.ts
+++ src/main/javascript/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MenuComponent} from './components/menu/menu.component';
+import { UserMenuComponent } from './components/user-menu/user-menu.component';
 
 
 @NgModule({
-    declarations: [MenuComponent],
+    declarations: [MenuComponent, UserMenuComponent],
     exports: [
         MenuComponent,
     ],
